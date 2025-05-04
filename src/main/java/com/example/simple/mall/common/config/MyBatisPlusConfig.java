5d561cd02954de3d0e91f13d0cfd17ec6022cd4c
Sssssssunny <12345678.@qq.com>
2025-05-05 02:08:22 +0900

    配置Mybatis-plus

---
+++ src/main/java/com/example/simple/mall/common/config/MyBatisPlusConfig.java
@@ -0,0 +1,25 @@
+package com.example.simple.mall.common.config;
+
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * MyBatisPlusConfig
+ *
+ * @author sunny
+ * @since 2025/05/05
+ */
+@Configuration
+@MapperScan("com.example.simple.mall.mapper")
+public class MyBatisPlusConfig {
+
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor());
+        return interceptor;
+    }
+}
