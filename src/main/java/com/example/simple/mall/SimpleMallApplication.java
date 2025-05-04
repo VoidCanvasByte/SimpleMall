ec012814faf61298ba0229bbd23af3eabe542df6
Sssssssunny <12345678.@qq.com>
2025-05-05 02:09:57 +0900

    完善代码

--- src/main/java/com/example/simple/mall/SimpleMallApplication.java
+++ src/main/java/com/example/simple/mall/SimpleMallApplication.java
@@ -1,11 +1,18 @@
-package com.example.simplemall;
+package com.example.simple.mall;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * SimpleMallApplication
+ *
+ * @author sunny
+ * @since 2025/05/05
+ */
+@MapperScan("com.example.simple.mall.api.mapper")
 @SpringBootApplication
 public class SimpleMallApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(SimpleMallApplication.class, args);
     }
