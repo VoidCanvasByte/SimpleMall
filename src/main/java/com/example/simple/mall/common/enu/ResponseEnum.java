975a41605d9595d917c18851b66503c44083bed7
Sssssssunny <12345678.@qq.com>
2025-05-05 02:09:40 +0900

    添加ResponseEnum 枚举

---
+++ src/main/java/com/example/simple/mall/common/enu/ResponseEnum.java
@@ -0,0 +1,27 @@
+package com.example.simple.mall.common.enu;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+/**
+ * 响应的枚举
+ *
+ * @author sunny
+ * @since 2025/05/05
+ */
+@Getter
+@AllArgsConstructor
+public enum ResponseEnum {
+
+    SUCCESS(200, "操作成功"),
+    FAIL(500, "操作失败"),
+    // 用户相关
+    USER_NOT_EXIST(1001, "用户不存在"),
+    USER_EXIST(1002, "当前用户邮箱已经注册，请确认邮箱信息"),
+    USER_PASSWORD_ERROR(1003, "用户名或密码错误，请确认信息"),
+    USER_NOT_LOGIN(1004, "用户未登录"),
+    USER_LOGIN_ERROR(1005, "用户登录失败"),
+    ;
+    private final Integer code;
+    private final String msg;
+}
