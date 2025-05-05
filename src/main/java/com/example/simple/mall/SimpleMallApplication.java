7972f455a2e509a3f1f9276035468b86b59141e4
Sssssssunny <12345678.@qq.com>
2025-05-05 13:10:10 +0900

    调整SimpleMallApplication的配置

--- src/main/java/com/example/simple/mall/SimpleMallApplication.java
+++ src/main/java/com/example/simple/mall/SimpleMallApplication.java
@@ -10,8 +10,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author sunny
  * @since 2025/05/05
  */
-@MapperScan("com.example.simple.mall.api.mapper")
 @SpringBootApplication
+@MapperScan("com.example.simple.mall.api.mapper")
 public class SimpleMallApplication {
     public static void main(String[] args) {
         SpringApplication.run(SimpleMallApplication.class, args);
