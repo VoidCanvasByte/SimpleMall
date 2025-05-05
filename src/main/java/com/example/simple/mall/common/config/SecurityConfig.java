6a74c7ca4593893ffb070fa8d8c71e50bd12d9bb
Sssssssunny <12345678.@qq.com>
2025-05-05 13:09:58 +0900

    添加SecurityConfig配置

---
+++ src/main/java/com/example/simple/mall/common/config/SecurityConfig.java
@@ -0,0 +1,37 @@
+package com.example.simple.mall.common.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+/**
+ * SecurityConfig的配置类（放行Swagger-UI用）
+ *
+ * @author sunny
+ * @since 2025/05/05
+ */
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.
+                csrf(AbstractHttpConfigurer::disable). // 关闭 CSRF（防止 Swagger 发 POST 被拦）
+                authorizeHttpRequests(auth -> auth
+                // 允许Swagger相关路径匿名访问
+                .requestMatchers(
+                        "/v3/api-docs/**",
+                        "/swagger-ui/**",
+                        "/swagger-ui.html",
+                        "/api/auth/**",
+                        "/apply/**")
+                .permitAll()
+                // 其他接口都需要认证
+                .anyRequest().authenticated());
+        return http.build();
+    }
+}
